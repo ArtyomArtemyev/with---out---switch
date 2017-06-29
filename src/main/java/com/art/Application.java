427330d870cd2660bb8eb03c44f966eb7a20e44b
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-06-29 21:22:25 +0300

    added controller

--- src/main/java/com/art/Application.java
+++ src/main/java/com/art/Application.java
@@ -2,10 +2,18 @@ package com.art;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class Application {
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String getHello() {
+        return "hello";
     }
 }
