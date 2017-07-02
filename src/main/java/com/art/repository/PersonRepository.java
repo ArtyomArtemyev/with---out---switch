f71f3cf0acfe42da6741d9ac7f5da27c7523a47c
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-07-02 21:58:14 +0300

    added repository for entity Person

---
+++ src/main/java/com/art/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.art.repository;
+
+
+import com.art.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+}
